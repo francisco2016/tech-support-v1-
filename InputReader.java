22aa1231d0facb61f6594c9de3adc49058deb820
francisco2016 <otrayama@gmail.com>
2016-03-16 11:27:31 +0100

    --0094 modificado mt getInput() responde en relacion con la palbra clave, aunque este entre varias palabras

--- InputReader.java
+++ InputReader.java
@@ -21,7 +21,7 @@ public class InputReader
         reader = new Scanner(System.in);
     }
 
-    /**
+   /**
      * Read a line of text from standard input (the text terminal),
      * ----------------------------Leer una línea de texto de la entrada estándar (el terminal de texto)
      * and return it as a String.
@@ -31,8 +31,13 @@ public class InputReader
     public String getInput()
     {
         System.out.print("> ");         // print prompt
-        String inputLine = reader.nextLine();
-
-        return inputLine;
+        String inputLine = reader.nextLine();//1ºen la VL inputLine recogemos lo que el usuaio pone en la terminal --    0094
+        inputLine.length();
+        
+        return inputLine.split(" ")[0];//solución en una línea al ---------------------------------------------------- 0094
+        //String[] ArrayStrings = inputLine.split(" ");//2ºrompemos la cadena en función de un espacio, con el mt split() ---0094
+        //String firstWord = ArrayStrings[0];   // 3º extraemos la 1º cadena "ArrayString[0]" y la guardo en "firstWord" ----0094
+        //return firstWord;     // ---------------------------------------------------------------------------------------- 0094
     }
+
 }
